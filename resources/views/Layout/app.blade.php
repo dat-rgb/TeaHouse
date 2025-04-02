c0c9625bbaed9f41db0a920f98ba3f8ea62e4674
tran-chi-dat <trdatt3737@gmail.com>
2025-04-02 16:31:35 +0700

    Them link icon logo, trang login admin

--- resources/views/Layout/app.blade.php
+++ resources/views/Layout/app.blade.php
@@ -8,7 +8,7 @@
     <meta content="" name="keywords">
     <meta content="" name="description">
     <!-- Favicon -->
-    <link href="{{asset('img/favicon.ico')}}" rel="icon">
+    <link href="{{asset('img/logo.png')}}" rel="icon">
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
